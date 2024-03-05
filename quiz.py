117d9d4f26ec47c0484426958de13ff0201f1b26
chiaragostino <chiara.agostino25@ycdsbk12.ca>
2024-03-05 10:04:56 -0500

    fixed code

--- quiz.py
+++ quiz.py
@@ -10,7 +10,11 @@ print("Multiple choice quiz game!\n")
 total_questions = 4
 questions_correct = 0
 
-question_one = input("What is my nick name?: \na) Chiara \nb) kiki \nc) chichi \nd) ki \n")
+question_one = input("What is my nick name?: \
+                     \na) Chiara \
+                     \nb) kiki \
+                     \nc) chichi \
+                     \nd) ki \n")
 
 if question_one == "b" or question_one == "B":
     print("Correct!\n")
@@ -18,7 +22,11 @@ if question_one == "b" or question_one == "B":
 else:
     print("Incorrect, the answer is kiki.\n")
 
-question_two = input("\nWhat year was I born?: \na) 2008 \nb) 2006 \nc) 2007 \nd) 2005 \n")
+question_two = input("What year was I born?: \
+                     \na) 2008 \
+                     \nb) 2006 \
+                     \nc) 2007 \
+                     \nd) 2005 \n")
 
 if question_two == "c" or question_one == "C":
     print("Correct!\n")
@@ -26,7 +34,11 @@ if question_two == "c" or question_one == "C":
 else:
     print("Incorrect, the answer is 2007.\n")
 
-question_three = input("What is my favourite number?: \na) 8 \nb) 11 \nc) 18 \nd) 14\n")
+question_three = input("What is my favourite number?: \
+                       \na) 8 \
+                       \nb) 11 \
+                       \nc) 18 \
+                       \nd) 14\n")
 
 if question_three == "a" or question_three == "A":
     print("Correct!\n")
@@ -34,7 +46,11 @@ if question_three == "a" or question_three == "A":
 else:
     print("Incorrect, the answer is 8.\n")
 
-question_four = input("What is my dogs name?: \na) Kernel \nb) Goldie \nc) Popcorn \nd) George\n")
+question_four = input("What is my dogs name?: \
+                      \na) Kernel \
+                      \nb) Goldie \
+                      \nc) Popcorn \
+                      \nd) George\n")
 
 if question_four == "c" or question_four == "C":
     print("Correct!\n")
